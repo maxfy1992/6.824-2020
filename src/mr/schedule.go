4e66e2d0dc8d2be7162d11ead13eeb8327149136
yangfengmax <yangfengmax@didichuxing.com>
2020-01-29 20:17:13 +0800

    fix data race for go func

--- src/mr/schedule.go
+++ src/mr/schedule.go
@@ -34,6 +34,7 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	// Your code here (Part III, Part IV).
 	//
 	// NOTE: wait until all tasks have completed, call wg.Done() when any task completes
+
 	constructTaskArgs := func(phase jobPhase, task int) DoTaskArgs {
 		debug("task: %d\n", task)
 		var taskArgs DoTaskArgs
@@ -53,38 +54,51 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	The master keep polling the number of finished tasks.
 	When all tasks have finished, master can break the loop and finish schedule
 	*/
+	/*
+		var wg sync.WaitGroup
+		for i := 0; i < ntasks; i++ {
+			wg.Add(1)
+			doTaskArgs := DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
+			go func(doTaskArgs DoTaskArgs, registerChan chan string) {
+				worker := <-registerChan
+				fmt.Printf("Assigning %v #%d task to %s\n", phase, doTaskArgs.TaskNumber, worker)
+				for !call(worker, "Worker.DoTask", doTaskArgs, nil) {
+					worker = <-registerChan
+				}
+				go func() {
+					registerChan <- worker
+				}()
+				wg.Done()
+			}(doTaskArgs, registerChan)
+		}
+		wg.Wait()
+	*/
+
 	tasks := make(chan int, ntasks) // act as task queue
-	//go func() {
 	for i := 0; i < ntasks; i++ {
 		tasks <- i
 	}
-	//}()
 	successTasks := 0
 	success := make(chan int)
-	/*
-		bug that can't stop from for select
-		var wg sync.WaitGroup
-		wg.Add(ntasks)
-	*/
+
 loop:
 	for {
 		select {
 		// TODO ? task 是否会复制一份到go func，task变量是否有并发访问问题？？
-		// go func 的闭包特性，是否可以显式指定哪些变量共享，哪些变量copy一份？
+		// go func 的闭包特性，是否可以显式指定哪些变量共享，哪些变量copy一份，看来channel可以直接共享没问题
 		case task := <-tasks:
-			go func() {
+			go func(doTaskArgs DoTaskArgs) {
 				worker := <-registerChan
-				fmt.Printf("Assigning %v #%d task to %s\n", phase, task, worker)
-				status := call(worker, "Worker.DoTask", constructTaskArgs(phase, task), nil)
+				fmt.Printf("Assigning %v #%d task to %s\n", doTaskArgs.Phase, doTaskArgs.TaskNumber, worker)
+				status := call(worker, "Worker.DoTask", doTaskArgs, nil)
 				if status {
-					fmt.Printf("Assigning %v #%d task to %s is done \n", phase, task, worker)
+					fmt.Printf("Assigning %v #%d task to %s is done \n", doTaskArgs.Phase, doTaskArgs.TaskNumber, worker)
 					success <- 1
 					go func() { registerChan <- worker }()
-					//wg.Done()
 				} else {
-					tasks <- task
+					tasks <- doTaskArgs.TaskNumber
 				}
-			}()
+			}(constructTaskArgs(phase, task))
 		case <-success:
 			successTasks += 1
 		default:
@@ -94,6 +108,5 @@ loop:
 		}
 	}
 
-	//wg.Wait()
 	fmt.Printf("Schedule: %v done\n", phase)
 }
